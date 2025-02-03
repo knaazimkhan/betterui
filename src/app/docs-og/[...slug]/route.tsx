1f454bb1b55a99fbc0db6d33cf8970a559eafdde
Naazim Khan <knaazimkhan@gmail.com>
2025-02-03 14:02:43 +0400

    fix: no need to  export const runtime = 'edge'

--- src/app/docs-og/[...slug]/route.tsx
+++ src/app/docs-og/[...slug]/route.tsx
@@ -2,8 +2,6 @@ import { generateOGImage } from 'fumadocs-ui/og'
 
 import { metadataImage } from '@/lib/metadata'
 
-export const runtime = 'edge'
-
 export const GET = metadataImage.createAPI((page) => {
   return generateOGImage({
     title: page.data.title,
