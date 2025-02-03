ddda6b4da318b65e54d972001f4a15f9c02be6bd
Naazim Khan <knaazimkhan@gmail.com>
2025-02-03 17:10:01 +0400

    fix: to cache the route handler response forever

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,6 +2,7 @@ import { createFromSource } from 'fumadocs-core/search/server'
 
 import { source } from '@/lib/source'
 
-export const runtime = 'edge'
+// it should be cached forever
+export const revalidate = false
 
 export const { GET } = createFromSource(source)
