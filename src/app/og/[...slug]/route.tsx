e42149ab9be0339cb0bd0e658caf396822dbacb1
Naazim Khan <knaazimkhan@gmail.com>
2025-02-04 11:42:51 +0400

    fix: nextjs runtime to edge

--- src/app/og/[...slug]/route.tsx
+++ src/app/og/[...slug]/route.tsx
@@ -2,6 +2,8 @@ import { generateOGImage } from 'fumadocs-ui/og'
 
 import { metadataImage } from '@/lib/metadata-image'
 
+export const runtime = 'edge'
+
 export const GET = metadataImage.createAPI((page) => {
   return generateOGImage({
     title: page.data.title,
