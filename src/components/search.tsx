e0c57e4f4053bc75e523d734f0aacea9afaf7f37
Naazim Khan <knaazimkhan@gmail.com>
2025-02-05 13:20:37 +0400

    update: orama cloud to built in search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-import { OramaClient } from '@oramacloud/client'
-import SearchDialog from 'fumadocs-ui/components/dialog/search-orama'
+import SearchDialog from 'fumadocs-ui/components/dialog/search-default'
 
 import type { SharedProps } from 'fumadocs-ui/components/dialog/search'
 
-const endpoint = process.env.NEXT_PUBLIC_ORAMA_ENDPOINT || ''
-const apiKey = process.env.NEXT_PUBLIC_ORAMA_PUBLIC_API_KEY || '' // public API key
-
-const client = new OramaClient({ endpoint: endpoint, api_key: apiKey })
-
 export default function CustomSearchDialog(props: SharedProps) {
-  return <SearchDialog {...props} client={client} showOrama />
+  return <SearchDialog {...props} />
 }
