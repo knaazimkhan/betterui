6b6c9bffbc9469db3690f9fc16bffb3f094bbc2f
Naazim Khan <knaazimkhan@gmail.com>
2025-02-05 12:52:27 +0400

    update: format only

--- source.config.ts
+++ source.config.ts
@@ -1,6 +1,4 @@
 import { defineDocs } from 'fumadocs-mdx/config'
 
 // define `docs` and `meta` collections
-export const { docs, meta } = defineDocs({
-  dir: 'content/docs',
-})
+export const { docs, meta } = defineDocs({ dir: 'content/docs' })
