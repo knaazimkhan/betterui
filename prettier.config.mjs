4a35f6680263ddb69b82df0fc0982b3d6acff61d
Naazim Khan <knaazimkhan@gmail.com>
2025-02-06 16:50:43 +0400

    feat: prettier plugin classnames, tailwindcss and merge added

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,6 @@
-/** @type {import("prettier").Config} */
+const functions = ['cn', 'clsx', 'cva']
 
+/** @type {import("prettier").Config} */
 const prettierConfig = {
   semi: false,
   singleQuote: true,
@@ -10,6 +11,13 @@ const prettierConfig = {
   bracketSpacing: true,
   arrowParens: 'always',
   endOfLine: 'lf',
+  tailwindFunctions: functions,
+  customFunctions: functions,
+  plugins: [
+    'prettier-plugin-classnames',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-merge',
+  ],
 }
 
 export default prettierConfig
--- src/components/component-wrapper.tsx
+++ src/components/component-wrapper.tsx
@@ -1,13 +1,12 @@
 'use client'
-
 interface ComponentWrapperProps {
   title: string
   children: React.ReactNode
 }
 export function ComponentWrapper({ title, children }: ComponentWrapperProps) {
   return (
-    <div className="bg-white/20 backdrop-blur-sm rounded-lg p-6 shadow-lg">
-      <h2 className="text-2xl font-semibold mb-4 text-white">{title}</h2>
+    <div className="rounded-lg bg-white/20 p-6 shadow-lg backdrop-blur-sm">
+      <h2 className="mb-4 text-2xl font-semibold text-white">{title}</h2>
       {children}
     </div>
   )
