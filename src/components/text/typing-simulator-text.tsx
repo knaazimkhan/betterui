00d1209f33dec50d66dc864129c7a23b3cf70cc6
Naazim Khan <knaazimkhan@gmail.com>
2025-02-10 19:41:03 +0400

    text: TypingSimulator added

---
+++ src/components/text/typing-simulator-text.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+export function TypingSimulator({ text }: { text: string }) {
+  const [displayText, setDisplayText] = useState('')
+
+  useEffect(() => {
+    let i = 0
+    const interval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayText((prev) => prev + text[i])
+        i++
+      } else {
+        clearInterval(interval)
+      }
+    }, 100)
+
+    return () => clearInterval(interval)
+  }, [text])
+
+  return (
+    <div className="rounded bg-gray-800 p-4 text-4xl font-bold text-white">
+      {displayText}
+      <motion.span
+        animate={{ opacity: [0, 1, 0] }}
+        transition={{ duration: 0.8, repeat: Number.POSITIVE_INFINITY }}
+      >
+        |
+      </motion.span>
+    </div>
+  )
+}
