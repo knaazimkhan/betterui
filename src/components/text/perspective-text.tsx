8b9f383151c9f3098e63e5bee9bd7e6e098fa669
Naazim Khan <knaazimkhan@gmail.com>
2025-02-11 13:18:28 +0400

    doc: PerspectiveText added

--- src/components/text/perspective-text.tsx
+++ src/components/text/perspective-text.tsx
@@ -2,7 +2,11 @@
 
 import { motion } from 'framer-motion'
 
-export function PerspectiveText({ text }: { text: string }) {
+export interface PerspectiveTextProps {
+  text: string
+}
+
+export function PerspectiveText({ text }: PerspectiveTextProps) {
   return (
     <div className="perspective-[1000px] rounded bg-gray-800 p-4 text-4xl font-bold text-white">
       <motion.div
