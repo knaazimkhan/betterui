f223adb74fc83a1292cda4bf0f00fbdb1261f7e3
Naazim Khan <knaazimkhan@gmail.com>
2025-02-11 17:33:12 +0400

    fix: lint issue

--- src/components/preview-component.tsx
+++ src/components/preview-component.tsx
@@ -23,7 +23,8 @@ export function PreviewComponent({
     <div className="relative">
       <Button
         onClick={() => setRefreshKey((prev: number) => prev + 1)}
-        className="absolute right-2 top-2 z-10 rounded-full p-2 transition-colors hover:bg-primary/10"
+        className="absolute right-2 top-2 z-10 rounded-full p-2 transition-colors
+          hover:bg-primary/10"
         variant="ghost"
         aria-label="Refresh Preview"
       >
@@ -32,11 +33,15 @@ export function PreviewComponent({
       <div
         key={refreshKey}
         className={cn(
-          `rounded-xl border bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-0 md:p-16`,
+          `rounded-xl border bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500
+          p-0 md:p-16`,
           className
         )}
       >
-        <div className="mx-auto flex max-w-6xl items-center justify-center rounded-xl bg-white/10 p-8 shadow-2xl backdrop-blur-md">
+        <div
+          className="mx-auto flex max-w-6xl items-center justify-center rounded-xl bg-white/10 p-8
+            shadow-2xl backdrop-blur-md"
+        >
           {children}
         </div>
       </div>
