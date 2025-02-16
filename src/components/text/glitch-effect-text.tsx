28cdb49f3017ffb6666d11d5f8856c6a01bb0424
Naazim Khan <knaazimkhan@gmail.com>
2025-02-16 18:00:20 +0400

    feat: added will-change-transform for better performance, and adjusted the z-index values so that the layers will stack correctly

--- src/components/text/glitch-effect-text.tsx
+++ src/components/text/glitch-effect-text.tsx
@@ -8,9 +8,9 @@ export interface GlitchEffectTextProps {
 
 export function GlitchEffectText({ text }: GlitchEffectTextProps) {
   return (
-    <div className="relative overflow-hidden rounded bg-gray-800 p-4 text-4xl font-bold text-white">
+    <div className="relative p-1 text-4xl font-bold text-white">
       <motion.div
-        className="absolute inset-0 text-red-500"
+        className="absolute inset-0 z-10 text-red-500 will-change-transform"
         animate={{
           x: [-2, 2, -2],
           y: [2, -2, 2],
@@ -24,7 +24,7 @@ export function GlitchEffectText({ text }: GlitchEffectTextProps) {
         {text}
       </motion.div>
       <motion.div
-        className="absolute inset-0 text-blue-500"
+        className="absolute inset-0 z-20 text-blue-500 will-change-transform"
         animate={{
           x: [2, -2, 2],
           y: [-2, 2, -2],
@@ -37,7 +37,7 @@ export function GlitchEffectText({ text }: GlitchEffectTextProps) {
       >
         {text}
       </motion.div>
-      <div className="relative">{text}</div>
+      <div className="relative z-30">{text}</div>
     </div>
   )
 }
