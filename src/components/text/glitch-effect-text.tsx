fc896dbfccde88bd4f5c89ce464e6aa260959163
Naazim Khan <knaazimkhan@gmail.com>
2025-02-16 18:14:40 +0400

    feat: Added glitchSpeed, glitchDirection, and glitchColors to allow for easy adjustments of the effect

--- src/components/text/glitch-effect-text.tsx
+++ src/components/text/glitch-effect-text.tsx
@@ -4,34 +4,46 @@ import { motion } from 'framer-motion'
 
 export interface GlitchEffectTextProps {
   text: string
+  glitchSpeed?: number
+  glitchDirection?: 'horizontal' | 'vertical'
+  glitchColors?: [string, string]
 }
 
-export function GlitchEffectText({ text }: GlitchEffectTextProps) {
+export function GlitchEffectText({
+  text,
+  glitchSpeed = 0.3,
+  glitchDirection = 'horizontal',
+  glitchColors = ['text-red-500', 'text-blue-500'],
+}: GlitchEffectTextProps) {
+  const direction = glitchDirection === 'horizontal' ? [-2, 2, -2] : [2, -2, 2]
+  const secondaryDirection =
+    glitchDirection === 'horizontal' ? [2, -2, 2] : [-2, 2, -2]
+
   return (
     <div className="relative p-1 text-4xl font-bold text-white">
       <motion.div
-        className="absolute inset-0 z-10 text-red-500 will-change-transform"
+        className={`absolute inset-0 ${glitchColors[0]} z-10 will-change-transform`}
         animate={{
-          x: [-2, 2, -2],
+          x: direction,
           y: [2, -2, 2],
         }}
         transition={{
           repeat: Number.POSITIVE_INFINITY,
-          duration: 0.3,
+          duration: glitchSpeed,
           ease: 'linear',
         }}
       >
         {text}
       </motion.div>
       <motion.div
-        className="absolute inset-0 z-20 text-blue-500 will-change-transform"
+        className={`absolute inset-0 ${glitchColors[1]} z-20 will-change-transform`}
         animate={{
-          x: [2, -2, 2],
+          x: secondaryDirection,
           y: [-2, 2, -2],
         }}
         transition={{
           repeat: Number.POSITIVE_INFINITY,
-          duration: 0.3,
+          duration: glitchSpeed,
           ease: 'linear',
         }}
       >
