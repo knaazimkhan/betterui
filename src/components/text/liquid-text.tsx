4c380e14a81d17c6534fe8bad2e19a3a446bb96a
Naazim Khan <knaazimkhan@gmail.com>
2025-02-17 14:50:51 +0400

    feat: separating the animation logic into variants

--- src/components/text/liquid-text.tsx
+++ src/components/text/liquid-text.tsx
@@ -6,18 +6,24 @@ export interface LiquidTextProps {
   text: string
 }
 
+const animationVariants = {
+  animate: {
+    borderRadius: ['20% 80% 20% 80%', '80% 20% 80% 20%', '20% 80% 20% 80%'],
+  },
+  transition: {
+    duration: 3,
+    repeat: Infinity,
+    ease: 'easeInOut',
+  },
+}
+
 export function LiquidText({ text }: LiquidTextProps) {
   return (
     <motion.div
       className="overflow-hidden rounded bg-blue-500 p-4 text-4xl font-bold text-white"
-      animate={{
-        borderRadius: ['20% 80% 20% 80%', '80% 20% 80% 20%', '20% 80% 20% 80%'],
-      }}
-      transition={{
-        duration: 3,
-        repeat: Number.POSITIVE_INFINITY,
-        ease: 'easeInOut',
-      }}
+      variants={animationVariants}
+      animate="animate"
+      transition={animationVariants.transition}
     >
       {text}
     </motion.div>
