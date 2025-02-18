ecf5593c913c99e21b6007c86e9c9d168858ef30
Naazim Khan <knaazimkhan@gmail.com>
2025-02-18 17:28:05 +0400

    feat: Allow customization via the text color, glow color and duration

--- src/components/text/neon-glow-text.tsx
+++ src/components/text/neon-glow-text.tsx
@@ -2,22 +2,32 @@
 
 import { motion } from 'framer-motion'
 
+import { cn } from '@/lib/utils'
+
 export interface NeonGlowTextProps {
   text: string
+  glowColor?: string
+  textColor?: string
+  duration?: number
 }
 
-export function NeonGlowText({ text }: NeonGlowTextProps) {
+export function NeonGlowText({
+  text,
+  glowColor = '#ff00de',
+  textColor = 'white',
+  duration = 1,
+}: NeonGlowTextProps) {
   return (
     <motion.div
-      className="rounded bg-black p-4 text-4xl font-bold text-white"
+      className={cn('text-4xl font-bold text-white', textColor)}
       animate={{
         textShadow: [
-          '0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff00de, 0 0 35px #ff00de, 0 0 40px #ff00de, 0 0 50px #ff00de, 0 0 75px #ff00de',
-          '0 0 2px #fff, 0 0 5px #fff, 0 0 7px #fff, 0 0 10px #ff00de, 0 0 17px #ff00de, 0 0 20px #ff00de, 0 0 25px #ff00de, 0 0 37px #ff00de',
+          `0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px ${glowColor}, 0 0 35px ${glowColor}, 0 0 40px ${glowColor}, 0 0 50px ${glowColor}, 0 0 75px ${glowColor}`,
+          `0 0 2px #fff, 0 0 5px #fff, 0 0 7px #fff, 0 0 10px ${glowColor}, 0 0 17px ${glowColor}, 0 0 20px ${glowColor}, 0 0 25px ${glowColor}, 0 0 37px ${glowColor}`,
         ],
       }}
       transition={{
-        duration: 1,
+        duration,
         repeat: Number.POSITIVE_INFINITY,
         repeatType: 'reverse',
       }}
