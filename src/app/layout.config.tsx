00c2492ef7fdd575083f83619910c6e23116a43b
Naazim Khan <knaazimkhan@gmail.com>
2025-03-02 17:48:32 +0400

    logo at navbar added

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
+
 import { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
 import { HomeIcon, LayoutPanelTop } from 'lucide-react'
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Better UI',
+    title: (
+      <div className="flex items-end justify-center gap-1">
+        <Image src="/logo.svg" alt="Better UI" width={25} height={25} />
+        <span className="font-bold text-primary">Better UI</span>
+      </div>
+    ),
+    transparentMode: 'top',
   },
   links: [
     {
